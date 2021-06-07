306f90d9edd4946fcbed16a26ee2a7e13ff61270
petershivachi <58518220+petershivachi@users.noreply.github.com>
2021-06-07 14:46:44 +0300

    Table and column component added

---
+++ client/src/components/Column.js
@@ -0,0 +1,14 @@
+export const column = [
+  {
+    Header: 'Category',
+    accessor: 'category',
+  },
+  {
+    Header: 'Sub-Category',
+    accessor: 'subCategory',
+  },
+  {
+    Header: 'state',
+    accessor: (prop) => prop.state,
+  },
+];
